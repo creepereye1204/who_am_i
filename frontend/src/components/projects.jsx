af37929c37ccbbac4e3855b655a9c1ac9bb822f8
creepereye <creepereye1204@gmail.com>
2024-11-18 10:34:25 +0000

    Commit on 2024-11-18 10:34:25

---
+++ frontend/src/components/projects.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+const Style = styled.div``;
+const Project = () => {
+  return (
+    <div className="container">
+      <div className="title"></div>
+      <div className="date"></div>
+      <div className="describe"></div>
+      <div className="content"></div>
+      <div className="link"></div>
+      <div className="skills"></div>
+    </div>
+  );
+};
+const Projects = () => {
+  return <Style></Style>;
+};
+export default Projects;
