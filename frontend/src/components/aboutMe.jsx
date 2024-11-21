50e6c1d3c8e4c748e4ad00f1d7d918023e7b8af2
creepereye <creepereye1204@gmail.com>
2024-11-21 15:03:28 +0000

    Commit on 2024-11-21 15:03:28

--- frontend/src/components/aboutMe.jsx
+++ frontend/src/components/aboutMe.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-
+import birthdate from "../icons/birthdate.svg";
+import education from "../icons/education.svg";
+import email from "../icons/email.svg";
+import location from "../icons/location.svg";
+import name from "../icons/name.svg";
+import phone from "../icons/phone.svg";
 const Style = styled.div`
   * {
     font-family: "Black Han Sans", sans-serif;
@@ -8,65 +13,96 @@ const Style = styled.div`
   #container {
     text-align: center;
     width: 100%;
+    padding-top: 3rem;
   }
   #content {
     max-width: 71.25rem;
     display: flex;
     flex-direction: row;
-    justify-content: center; /* 내용물 가운데 정렬 */
+    justify-content: center;
+    margin: 0 auto;
     flex-wrap: wrap;
-    gap: 1rem; /* 아이콘 간격 */
   }
   #title {
     font-size: 2.5rem;
     font-weight: bolder;
   }
-  #describe {
+  #bar {
+    width: 10rem;
+    border: 0.1rem solid grey;
   }
   .icons {
-    display: flex; /* 아이콘을 가로로 배열 */
+    display: flex;
+    margin: 2rem 5rem;
 
-    justify-content: center; /* 중앙 정렬 */
-    margin: 20px; /* 아이콘 간격 */
+    column-gap: 2rem;
+  }
+  .icon {
+    width: 2.5rem;
+    height: auto;
+    opacity: 0.8;
+  }
+  .text {
+    text-align: start;
+    width: 5rem;
   }
 `;
 
 const Icon = ({ data }) => {
-  const { src, label, info } = data; // data 객체에서 값 추출
+  const { src, label, info } = data;
 
   return (
-    <div className="icons">
-      <img src={src} alt={label} />
-      <b>{label}</b>
-      <div>{info}</div>
-    </div>
+    <card className="icons">
+      <div>
+        <img src={src} alt={label} className="icon" />
+      </div>
+      <div className="text">
+        <div>{label}</div>
+        <div>{info}</div>
+      </div>
+    </card>
   );
 };
 
 const AboutMe = () => {
   const iconsData = [
     {
-      src: "image1.png",
-      label: "아이콘 1",
-      info: "정보 설명 1",
+      src: birthdate,
+      label: "생일",
+      info: "1934-10-04(틀딱)",
     },
     {
-      src: "image2.png",
-      label: "아이콘 2",
-      info: "정보 설명 2",
+      src: education,
+      label: "학력",
+      info: "삼육대학교(인공지능 융합학부)",
     },
     {
-      src: "image3.png",
-      label: "아이콘 3",
-      info: "정보 설명 3",
+      src: email,
+      label: "이메일",
+      info: "maratanghuru@gmail.com",
     },
-    // 추가 아이콘 데이터...
+    {
+      src: location,
+      label: "위치",
+      info: "파인하우스",
+    },
+    {
+      src: name,
+      label: "이름",
+      info: "빅랩",
+    },
+    {
+      src: phone,
+      label: "전화번호",
+      info: "010-1234-5678",
+    },
   ];
 
   return (
     <Style>
       <div id="container">
         <div id="title">ABOUT ME</div>
+        <hr id="bar"></hr>
         <div id="content">
           {iconsData.map((data, index) => (
             <Icon key={index} data={data} /> // Icon 컴포넌트 반복 렌더링
